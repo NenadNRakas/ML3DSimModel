faf08a6eb16d9033b0b3cbb172c56c6f94e038b0
Nenad Rakas <NenadRakas@outlook.com>
2020-03-23 15:47:32 +0200

    Initial commit

---
+++ Assets/Penguin/Scripts/PenguinAcademy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using MLAgents.Sensor;
+public class PenguinAcademy : Academy
+{
+    /// <summary>
+    /// Gets/sets the current fish speed
+    /// </summary>
+    public float FishSpeed { get; private set; }
+
+    /// <summary>
+    /// Gets/sets the current acceptable feed radius
+    /// </summary>
+    public float FeedRadius { get; private set; }
+    //public float FloatProperties;
+    /// <summary>
+    /// Called when the academy first gets initialized
+    /// </summary>
+    public override void InitializeAcademy()
+    {
+        FishSpeed = 0f;
+        FeedRadius = 0f;
+
+        // Set up code to be called every time the fish_speed parameter changes 
+        // during curriculum learning
+        //FloatProperties.RegisterCallback("fish_speed", f => { FishSpeed = f; });
+        //FloatProperties.RegisterCallback("fish_speed", f => { FishSpeed = f; });
+
+        // Set up code to be called every time the feed_radius parameter changes 
+        // during curriculum learning
+        //FloatProperties.RegisterCallback("feed_radius", f => { FeedRadius = f; });
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# ML3DSimModel
+ C#, Machine Learning, 3D Simulator, Cinematics, Animation Traning Model, +/- 70000 Steps, Microsoft Visual Studio Code, Windows 10 Pro, Unity Engine 2019.3, Baracuda, Python, Anaconda, TF, RNN, ML Agents
